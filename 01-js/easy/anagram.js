ec958bff751d81782c4801b39f49e75cb069e45f
HarshGoel22 <hgoel0260@gmail.com>
2024-11-09 11:57:00 +0530

    intial commit

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,20 @@
 */
 
 function isAnagram(str1, str2) {
+  if(str1.length != str2.length) return false;
+  str1 = str1.toLowerCase(), str2 = str2.toLowerCase();
+  let list = {};
+  for(var i=0; i<str1.length; i++){
+    if(!list[str1[i]]) list[str1[i]]=1;
+    else list[str1[i]]+=1;
+  };
 
+  for(var i=0; i<str2.length; i++){
+    if(list[str2[i]]==0 || !list[str2[i]]) return false;
+    else list[str2[i]]-=1;
+  };
+
+  return true;
 }
 
 module.exports = isAnagram;
